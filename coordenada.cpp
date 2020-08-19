f8b76aead3dded161b9c6133497e978e07d11fe4
Mariana R <Mariana_Rr@MacBook-Air-de-Mayra.local>
2020-08-18 19:30:26 -0500

    Cuadrciula

---
+++ coordenada.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include<string>
+
+int main (){
+
+    struct Coordenada{
+    int celula;
+	int x {0};
+    int y{0};
+    
+    };
+
+    int x=0;
+    int y=0;
+    int tamaño {10};
+    int ejex {0};
+    int ejey {0};
+    char celula {'#'};
+    char espacio {' '};
+    std::cout<< "Ingrese las coordenadas de la celula que quiere activar/n X: ";
+    std::cin >> x;
+    std::cout<< "Y:";
+    std::cin >> y;
+    
+    for (ejex=0; ejex<=tamaño; ++ejex){
+        if (ejex<tamaño){
+            std::cout<< "x"<<ejex<<" ";
+            } 
+        }
+
+    for (ejey=0; ejey<=tamaño;++ejey){
+            if (ejey<y){
+            std::cout<<"y"<<ejey<<"\n";
+            }
+            else if (ejey==y)
+            {
+                std::cout<<"y"<<ejey;
+                for (ejex=0; ejex<=x; ++ejex){
+                    std::cout<< espacio;
+                    if (ejex<x)
+                        {4
+                        std::cout<<" ";
+                        }
+                    else if (ejex==x)
+                        {
+                        std::cout<<'#'<<"\n";
+                        }   
+                    }
+            }
+
+                
+        
+        }
+    
+
+    return 0;
+    }
+
