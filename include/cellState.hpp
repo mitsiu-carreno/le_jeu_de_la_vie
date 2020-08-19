101dd426fabf886e2c6fd036307b841ff93fdd1a
alejandroCarreno <alejandro.carreno@data4.mx>
2020-08-18 20:17:48 -0500

    define cell struct

--- include/cellState.hpp
+++ include/cellState.hpp
@@ -2,7 +2,7 @@
 #define CELL_STATE_H
 
 struct CellState{
-  char alive[4] = "\u2588";
+  char alive[4] = "\u2589";
   char dead[2] = " "; 
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,47 @@
 #include "cellState.hpp"
 #include <iostream>
+#include <array>
+
+/*
+-Estado: vivo/muerto - *bool
+-Posición: (3,5) - *vector *array
+*/
+
+struct Cell{
+  bool status;
+  std::array<int,2> coor;
+  //int coor_x;
+  //int coor_y;
+};
+
+void PrintStatus(bool cell_status){
+
+  CellState cell_state;
+  if(cell_status == 1){
+    std::cout << cell_state.alive << "\n";
+  }else{
+    std::cout << cell_state.dead  << "\n";
+  }
+}
 
 int main(){
+  Cell celula;
+  celula.status = true;
+  celula.coor[0] = 6;
+  celula.coor[1] = 3;
 
+
+  Cell celula2;
+  celula2.status = false;
+  celula2.coor[0] = 5;
+  celula2.coor[1] = 3;
+
+  PrintStatus(celula.status);
+  PrintStatus(celula2.status);
+  /*
   CellState cell;
-  std::cout << cell.alive << cell.dead << "\n";
+  std::cout << cell.alive << cell.dead << cell.alive << "\n";
+  */
 
 	std::cout << "Oh la la\n";
 	return 0;
