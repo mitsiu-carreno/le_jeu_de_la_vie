010f48fc4d405a0fd3ba3e14c38d565b97be665d
alejandroCarreno <alejandro.carreno@data4.mx>
2020-08-25 20:59:00 -0500

    create cell_grid and instantiate each cell

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "windowLength.hpp"
 #include <iostream>
 #include <array>
+#include <vector>
 /*
 -Estado: vivo/muerto - *bool
 -Posición: (3,5) - *vector *array
@@ -10,6 +11,7 @@
 struct Cell{
   bool status;
   std::array<int,2> coor;
+  int id;
   //int coor_x;
   //int coor_y;
 };
@@ -26,36 +28,33 @@ void PrintStatus(bool cell_status){
 
 int main(){
   int side_grid = 5;
-  int size_grid = side_grid * side_grid;
+  //int size_grid = side_grid * side_grid;
 
   std::vector<std::vector<Cell>> cell_grid (side_grid);
+  int contador_id = 1;
   for(size_t i{0}; i<side_grid; ++i){
     cell_grid[i].resize(side_grid);
-    
+
     for(size_t j{0}; j<side_grid; ++j){
+      Cell cell;
       cell.status = true;
-      cell.coor = [i],[j];
+      //cell.coor = [i],[j];
+      cell.coor[0] = i;
+      cell.coor[1] = j;
+      cell.id = contador_id;
+      cell_grid[i][j] = cell;
+      ++contador_id;
     }
   }
   //std::vector<Cell> cell_grid (size_grid);
-  // b
+
   
-  // a
-
-
-  Cell celula;
-  celula.status = true;
-  celula.coor[0] = 6;
-  celula.coor[1] = 3;
-
-
-  Cell celula2;
-  celula2.status = false;
-  celula2.coor[0] = 5;
-  celula2.coor[1] = 3;
-
-  PrintStatus(celula.status);
-  PrintStatus(celula2.status);
+  for(size_t i{0}; i<cell_grid.size(); ++i){
+    for(size_t j{0}; j<cell_grid[i].size(); ++j){
+      std::cout << cell_grid[i][j].status << " id: " << cell_grid[i][j].id << " - " << cell_grid[i][j].coor[0] << "," << cell_grid[i][j].coor[1] << "\n";
+      //PrintStatus(cell_grid[i][j].status);
+    }
+  }
   /*
   CellState cell;
   std::cout << cell.alive << cell.dead << cell.alive << "\n";
