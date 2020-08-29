83e4c216759034b9f0a604425f6994d2e1dd8a98
Mariana R <Mariana_Rr@MacBook-Air-de-Mayra.local>
2020-08-28 21:07:29 -0500

    initialstatus

---
+++ src/initialState.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+#include <vector>
+
+
+
+void DefineInitialStatus (int cell_gridsize){
+
+int status_div {0};
+
+std::cout << "Para definir el estatus inicial de las celulas, escoja un número del 1 al"<< cell_gridsize<<"\n"; 
+std::cin>> status_div;
+
+//for para entrar cada una de las células 
+
+if (cell.id/status_div=0)
+{
+    cell.status=1;
+} else
+{
+    cell.status=0;
+}
+}
+
+
+
