39c0a8bff19027868be190d965484a6e496b2544
edgar_eponce@hotmail.com <edgar_eponce@hotmail.com>
2020-10-13 20:05:17 -0500

    sync

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,32 @@
 #include <iostream>
 #include <vector>
 
+int CellNewStatus(int cell_status, int status_vecinos) {
+    int new_cell_status;
+    if (cell_status == 1) {
+        switch (status_vecinos) {
+            case 2:
+                new_cell_status = 1;
+                break;
+            case 3:
+                new_cell_status = 1;
+            default:
+                new_cell_status = 0;
+        }
+    }
+    if (cell_status == 0) {
+        switch (status_vecinos) {
+        case 2:
+            new_cell_status = 1;
+            break;
+        case 3:
+            new_cell_status = 1;
+        default:
+            new_cell_status = 0;
+        }
+    }
+    return new_cell_status;
+}
 
 void CheckCellStatus(std::vector<std::vector<cell::Cell>> grid){ 
 //
@@ -56,61 +82,88 @@ int main(){
 
   for (size_t i{ 0 }; i < cell_grid.size(); ++i) {
       for (size_t j{ 0 }; j < cell_grid.size(); ++j) {
-          std::cout << "ID: "<< cell_grid[i][j].id <<" ";
-          int size { static_cast<int>(cell_grid.size())};
+          int size { static_cast<int>(cell_grid.size())}; // obtiene el tamaño de la matriz para futuros calculos
 
           if (cell_grid [i][j].id == 1) { //esquina arriba-izquierda
-              int temp;
-              temp = cell_grid[i][j + 1].status + cell_grid[i+1][j + 1].status + cell_grid[i+1][j].status;
-             
-              std::cout << temp <<"  ";
+              int suma_status_vecinos;
+              int cell_status{ cell_grid[i][j].status };
+              suma_status_vecinos = cell_grid[i][j + 1].status + cell_grid[i+1][j + 1].status + cell_grid[i+1][j].status;
+
+              std::cout << cell_grid[i][j].status <<"  ";
+              cell_grid[i][j].status = CellNewStatus(cell_status,suma_status_vecinos);
+              std::cout << cell_grid[i][j].status << "  ";
           }
           else if (cell_grid [i][j].id > 1 && cell_grid [i][j].id < size) { // central arriba
-              int temp;
-              temp = cell_grid[i][j + 1].status + cell_grid[i + 1][j + 1].status + cell_grid[i + 1][j].status + cell_grid[i + 1][j-1].status + cell_grid[i][j - 1].status;
+              int suma_status_vecinos;
+              int cell_status{ cell_grid[i][j].status };
+              suma_status_vecinos = cell_grid[i][j + 1].status + cell_grid[i + 1][j + 1].status + cell_grid[i + 1][j].status + cell_grid[i + 1][j-1].status + cell_grid[i][j - 1].status;
 
-              std::cout << temp << "  ";;
+              std::cout << cell_grid[i][j].status << "  ";
+              cell_grid[i][j].status = CellNewStatus(cell_status, suma_status_vecinos);
+              std::cout << cell_grid[i][j].status << "  ";
           }
           else if (cell_grid [i][j].id == size) { // esquina arriba-derecha
-              int temp;
-              temp = cell_grid[i][j - 1].status + cell_grid[i + 1][j - 1].status + cell_grid[i + 1][j].status;
+              int suma_status_vecinos;
+              int cell_status{ cell_grid[i][j].status };
+              suma_status_vecinos = cell_grid[i][j - 1].status + cell_grid[i + 1][j - 1].status + cell_grid[i + 1][j].status;
 
-              std::cout << temp << "  ";
+              std::cout << cell_grid[i][j].status << "  ";
+              cell_grid[i][j].status = CellNewStatus(cell_status, suma_status_vecinos);
+              std::cout << cell_grid[i][j].status << "  ";
           }
           else if (cell_grid [i][j].id == size * size - (size - 1)) { // esquina abajo-izquierda
-              int temp;
-              temp = cell_grid[i-1][j].status + cell_grid[i - 1][j + 1].status + cell_grid[i][j+1].status;
+              int suma_status_vecinos;
+              int cell_status{ cell_grid[i][j].status };
+              suma_status_vecinos = cell_grid[i-1][j].status + cell_grid[i - 1][j + 1].status + cell_grid[i][j+1].status;
 
-              std::cout << temp << "  ";
+              std::cout << cell_grid[i][j].status << "  ";
+              cell_grid[i][j].status = CellNewStatus(cell_status, suma_status_vecinos);
+              std::cout << cell_grid[i][j].status << "  ";
           }
           else if (cell_grid [i][j].id > size* size - (size - 1) && cell_grid [i][j].id < size * size) {  // cental abajo
-              int temp;
-              temp = cell_grid[i][j-1].status + cell_grid[i-1][j-1].status + cell_grid[i-1][j].status + cell_grid[i-1][j+1].status + cell_grid[i][j+1].status;
+              int suma_status_vecinos;
+              int cell_status{ cell_grid[i][j].status };
+              suma_status_vecinos = cell_grid[i][j-1].status + cell_grid[i-1][j-1].status + cell_grid[i-1][j].status + cell_grid[i-1][j+1].status + cell_grid[i][j+1].status;
 
-              std::cout << temp << "  ";
+              std::cout << cell_grid[i][j].status << "  ";
+              cell_grid[i][j].status = CellNewStatus(cell_status, suma_status_vecinos);
+              std::cout << cell_grid[i][j].status << "  ";
           }
           else if (cell_grid [i][j].id == size * size) { // esquina abajo-derecha
-              int temp;
-              temp = cell_grid[i][j-1].status + cell_grid[i-1][j-1].status + cell_grid[i-1][j].status;
+              int suma_status_vecinos;
+              int cell_status{ cell_grid[i][j].status };
+              suma_status_vecinos = cell_grid[i][j-1].status + cell_grid[i-1][j-1].status + cell_grid[i-1][j].status;
 
-              std::cout << temp << "  ";
+              std::cout << cell_grid[i][j].status << "  ";
+              cell_grid[i][j].status = CellNewStatus(cell_status, suma_status_vecinos);
+              std::cout << cell_grid[i][j].status << "  ";
           }
           else if (i > 0 && i < cell_grid.size()-1 && j == 0) { //  lateral izquierda
-              int temp;
-              temp = cell_grid[i-1][j].status + cell_grid[i-1][j+1].status + cell_grid[i][j+1].status + cell_grid[i+1][j+1].status + cell_grid[i+1][j].status;
+              int suma_status_vecinos;
+              int cell_status{ cell_grid[i][j].status };
+              suma_status_vecinos = cell_grid[i-1][j].status + cell_grid[i-1][j+1].status + cell_grid[i][j+1].status + cell_grid[i+1][j+1].status + cell_grid[i+1][j].status;
 
-              std::cout << temp << "  ";
+              std::cout << cell_grid[i][j].status << "  ";
+              cell_grid[i][j].status = CellNewStatus(cell_status, suma_status_vecinos);
+              std::cout << cell_grid[i][j].status << "  ";
           }
           else if (i > 0 && i < cell_grid.size() - 1 && j == size-1) {  // lateral derecha 
-              int temp;
-              temp = cell_grid[i-1][j].status + cell_grid[i-1][j-1].status + cell_grid[i][j-1].status + cell_grid[i+1][j-1].status + cell_grid[i+1][j].status;
+              int suma_status_vecinos;
+              int cell_status{ cell_grid[i][j].status };
+              suma_status_vecinos = cell_grid[i-1][j].status + cell_grid[i-1][j-1].status + cell_grid[i][j-1].status + cell_grid[i+1][j-1].status + cell_grid[i+1][j].status;
 
-              std::cout << temp << "  ";
+              std::cout << cell_grid[i][j].status << "  ";
+              cell_grid[i][j].status = CellNewStatus(cell_status, suma_status_vecinos);
+              std::cout << cell_grid[i][j].status << "  ";
           }
           else {
-              int temp;
-              temp = cell_grid[i][j + 1].status + cell_grid[i][j - 1].status + cell_grid[i + 1][j - 1].status + cell_grid[i + 1][j].status + cell_grid[i + 1][j + 1].status + cell_grid[i - 1][j + 1].status + cell_grid[i - 1][j].status + cell_grid[i - 1][j - 1].status;
-              std::cout << temp << "  ";
+              int suma_status_vecinos;
+              int cell_status{ cell_grid[i][j].status };
+              suma_status_vecinos = cell_grid[i][j + 1].status + cell_grid[i][j - 1].status + cell_grid[i + 1][j - 1].status + cell_grid[i + 1][j].status + cell_grid[i + 1][j + 1].status + cell_grid[i - 1][j + 1].status + cell_grid[i - 1][j].status + cell_grid[i - 1][j - 1].status;
+              
+              std::cout << cell_grid[i][j].status << "  ";
+              cell_grid[i][j].status = CellNewStatus(cell_status, suma_status_vecinos);
+              std::cout << cell_grid[i][j].status << "  ";
           }
       }
       std::cout << "\n";
